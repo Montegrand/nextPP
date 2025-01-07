803f8dceda54863842f89ae4e036474f246aa63e
Montegrand <hpt0311@naver.com>
2025-01-07 21:11:35 +0900

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/nextPP',
 };
 
 export default nextConfig;
---
+++ src/app/testPj1/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div id="wrapper">
+        각 분기별 test 1번
+    </div>
+  );
+}
